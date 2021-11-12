19a2ec67e36264c47aaacef38cc4cc3a558b4333
OorfeneDeuce <oorfened@gmail.com>
2021-11-12 07:37:23 +0000

    refactor: remove console.dir from code

--- functions/index.js
+++ functions/index.js
@@ -12,7 +12,6 @@ const verifyUser = async (token) => {
   }
   try {
     const decoded = await admin.auth().verifyIdToken(token);
-    console.dir(decoded);
     const user = await admin.auth().getUser(decoded.user_id);
     if (!user) {
       result.statusCode = 401;
@@ -31,131 +30,128 @@ exports.addTodo = functions
     .region("europe-west1")
     .https
     .onRequest(async (req, res) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  try {
-    const bodyData = JSON.parse(req.body);
-    const user = await verifyUser(bodyData.token);
-    const writeResult = await admin.firestore().collection("todos").add({
-      content: {
-        title: bodyData.title,
-      },
-      status: false,
-      created: Date.now(),
-      user_id: user.uid,
+      res.setHeader("Access-Control-Allow-Origin", "*");
+      try {
+        const bodyData = JSON.parse(req.body);
+        const user = await verifyUser(bodyData.token);
+        const writeResult = await admin.firestore().collection("todos").add({
+          content: {
+            title: bodyData.title,
+          },
+          status: false,
+          created: Date.now(),
+          user_id: user.uid,
+        });
+        res.setHeader("Access-Control-Allow-Origin", "*");
+        res.json({
+          result: `Message with ID: ${writeResult.id} added.`,
+          data: {id: writeResult.id},
+        });
+      } catch (error) {
+        if (error.statusCode) {
+          res.status(error.statusCode).json(error.response);
+        } else {
+          console.error(error);
+          res.sendStatus(500);
+        }
+      }
     });
-    console.dir(writeResult);
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.json({
-      result: `Message with ID: ${writeResult.id} added.`,
-      data: {id: writeResult.id},
-    });
-  } catch (error) {
-    if (error.statusCode) {
-      res.status(error.statusCode).json(error.response);
-    } else {
-      console.error(error);
-      res.sendStatus(500);
-    }
-  }
-});
 exports.updateTodo = functions
     .region("europe-west1")
     .https
     .onRequest(async (req, res) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  try {
-    const bodyData = JSON.parse(req.body);
-    const user = await verifyUser(bodyData.token);
-    const ref = admin.firestore().collection("todos").doc(bodyData.todoId);
-    console.dir(ref);
-    try {
-      await admin.firestore().runTransaction(async (t) => {
-        const doc = await t.get(ref);
-        if (doc.data().user_id !== user.uid) {
-          throw (new Error("No access"));
-        } else {
-          await t.update(ref, {status: bodyData.status});
+      res.setHeader("Access-Control-Allow-Origin", "*");
+      try {
+        const bodyData = JSON.parse(req.body);
+        const user = await verifyUser(bodyData.token);
+        const ref = admin.firestore().collection("todos").doc(bodyData.todoId);
+        try {
+          await admin.firestore().runTransaction(async (t) => {
+            const doc = await t.get(ref);
+            if (doc.data().user_id !== user.uid) {
+              throw (new Error("No access"));
+            } else {
+              await t.update(ref, {status: bodyData.status});
+            }
+          });
+          res.json({result: "update done"});
+        } catch (error) {
+          console.error("Access denied for user " + user.uid);
+          throw (error);
         }
-      });
-      res.json({result: "update done"});
-    } catch (error) {
-      console.error("Access denied for user " + user.uid);
-      throw (error);
-    }
-  } catch (error) {
-    if (error.statusCode) {
-      res.status(error.statusCode).json(error.response);
-    } else {
-      console.error(error);
-      res.sendStatus(500);
-    }
-  }
-});
+      } catch (error) {
+        if (error.statusCode) {
+          res.status(error.statusCode).json(error.response);
+        } else {
+          console.error(error);
+          res.sendStatus(500);
+        }
+      }
+    });
 
 exports.getTodos = functions
     .region("europe-west1")
     .https
     .onRequest(async (req, res) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  try {
-    const bodyData = JSON.parse(req.body);
-    const user = await verifyUser(bodyData.token);
-    const snapshot = await admin.firestore().collection("todos")
-        .where("user_id", "==", user.uid).get();
-    const data = [];
-    snapshot.forEach((doc) => {
-      const docData = doc.data();
-      data.push({
-        id: doc.id,
-        content: docData.content,
-        status: docData.status,
-      });
+      res.setHeader("Access-Control-Allow-Origin", "*");
+      try {
+        const bodyData = JSON.parse(req.body);
+        const user = await verifyUser(bodyData.token);
+        const snapshot = await admin.firestore().collection("todos")
+            .where("user_id", "==", user.uid).get();
+        const data = [];
+        snapshot.forEach((doc) => {
+          const docData = doc.data();
+          data.push({
+            id: doc.id,
+            content: docData.content,
+            status: docData.status,
+          });
+        });
+        if (data.length > 0) {
+          res.json({result: `${data.length} items finded and returned`, data});
+        } else {
+          res.json({result: "No data for specified user finded", data: []});
+        }
+      } catch (error) {
+        if (error.statusCode) {
+          res.status(error.statusCode).json(error.response);
+        } else {
+          console.error(error);
+          res.sendStatus(500);
+        }
+      }
     });
-    if (data.length > 0) {
-      res.json({result: `${data.length} items finded and returned`, data});
-    } else {
-      res.json({result: "No data for specified user finded", data: []});
-    }
-  } catch (error) {
-    if (error.statusCode) {
-      res.status(error.statusCode).json(error.response);
-    } else {
-      console.error(error);
-      res.sendStatus(500);
-    }
-  }
-});
 
 exports.deleteTodo = functions
     .region("europe-west1")
     .https
     .onRequest(async (req, res) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  try {
-    const bodyData = JSON.parse(req.body);
-    const user = await verifyUser(bodyData.token);
-    const ref = admin.firestore().collection("todos").doc(bodyData.todoId);
-    console.dir(ref);
-    try {
-      await admin.firestore().runTransaction(async (t) => {
-        const doc = await t.get(ref);
-        if (doc.data().user_id !== user.uid) {
-          throw (new Error("No access"));
-        } else {
-          await t.delete(ref);
+      res.setHeader("Access-Control-Allow-Origin", "*");
+      try {
+        const bodyData = JSON.parse(req.body);
+        const user = await verifyUser(bodyData.token);
+        const ref = admin.firestore().collection("todos").doc(bodyData.todoId);
+        try {
+          await admin.firestore().runTransaction(async (t) => {
+            const doc = await t.get(ref);
+            if (doc.data().user_id !== user.uid) {
+              throw (new Error("No access"));
+            } else {
+              await t.delete(ref);
+            }
+          });
+          res.json({result: "deletion done"});
+        } catch (error) {
+          console.error("Access denied for user " + user.uid);
+          throw (error);
         }
-      });
-      res.json({result: "deletion done"});
-    } catch (error) {
-      console.error("Access denied for user " + user.uid);
-      throw (error);
-    }
-  } catch (error) {
-    if (error.statusCode) {
-      res.status(error.statusCode).json(error.response);
-    } else {
-      console.error(error);
-      res.sendStatus(500);
-    }
-  }
-});
+      } catch (error) {
+        if (error.statusCode) {
+          res.status(error.statusCode).json(error.response);
+        } else {
+          console.error(error);
+          res.sendStatus(500);
+        }
+      }
+    });
